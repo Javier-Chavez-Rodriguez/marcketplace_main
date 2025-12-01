0063593f8b8052fb64d7c6710daa176acf047a98
Javier-Chavez-Rodriguez <javi0920868@gmail.com>
2025-12-01 07:26:05 -0800

    Comit inicial del proyecto

---
+++ store/forms.py
@@ -0,0 +1,88 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+from .models import Item
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'placeholder': 'Tu username',
+            'class': 'form-control'
+        }
+    ))
+
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'placeholder': 'Tu password',
+            'class': 'form-control'
+        }
+    ))
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2' ]
+
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'placeholder': 'Tu Username',
+            'class': 'form-control'
+        }
+    ))
+
+    email = forms.CharField(widget=forms.EmailInput(
+        attrs={
+            'placeholder': 'Tu Email',
+            'class': 'form-control'
+        }
+    ))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'placeholder': 'Password',
+            'class': 'form-control'
+        }
+    ))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'placeholder': 'Repite Tu Password',
+            'class': 'form-control'
+        }
+    ))
+
+class NewItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ['category', 'name', 'description', 'price', 'image']
+            
+        widgets = {
+            'category': forms.Select(
+                attrs={
+                    'class': 'form-select'
+                }
+            ),
+            'name': forms.TextInput(
+                attrs={
+                    'class': 'form-select'
+                }
+            ),
+            'description': forms.Textarea(
+                attrs={
+                    'class': 'form-select',
+                    'style': 'height: 100px'
+                }
+            ),
+            'price': forms.TextInput(
+                attrs={
+                'class': 'form-select'
+                }
+            ),
+            'image': forms.FileInput(
+                attrs={
+                    'class': 'form-select'
+                }
+            )  
+                
+        }
